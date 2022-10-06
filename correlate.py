b711a82e34e45eb5e61a17628caa25cae999e8e2
Vyas Raina <sayvaniar@gmail.com>
2022-10-06 18:32:30 +0100

    correlation

--- correlate.py
+++ correlate.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     name1 = name1.split('/')[-1].split('_')[0]
     name2 = args.perts2
     name2 = name2.split('/')[-1].split('_')[0]
-    data = np.stack(np.array(p1), np.array(p2))
+    data = np.stack((np.array(p1), np.array(p2)), axis=0)
     sns.jointplot(data, kind='reg')
     plt.xlabel(name1)
     plt.ylabel(name2)
